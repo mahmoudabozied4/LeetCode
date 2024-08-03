4a517bf17d2162e0d7be678756248b6c2ac0cabe
Mahmoud Abo-Zied <116457362+mahmoudabozied4@users.noreply.github.com>
2024-08-03 07:31:27 +0300

    [137] [Time Beats: 90.01%] [Memory Beats: 56.84%] - LeetPush

--- test/137-Single-Number-II.cpp
+++ test/137-Single-Number-II.cpp
@@ -1,14 +1,11 @@
 class Solution {
- public:
-  int singleNumber(vector<int>& nums) {
-    int ans = 0;
-    for (int i = 0; i < 32; ++i) {
-      int sum = 0;
-      for (auto num : nums)
-        sum +=( num >> i) & 1;
-      sum %= 3;
-      ans |= sum << i;
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones = 0, twos = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            ones = (ones ^ nums[i]) & ~twos;
+            twos = (twos ^ nums[i]) & ~ones;
+        }
+        return ones;
     }
-    return ans;
-  }
 };
